d673d9d373c10c0c9812be042c6acd7b4b41504c
Macon Suckow <msuckow@giveinteractive.com>
2025-02-17 15:54:45 -0600

    Adding yank/post config

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -11,3 +11,13 @@
 -- 		-- end
 -- 	end),
 -- })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+		local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
+		copy_to_unnamedplus(vim.v.event.regcontents)
+		local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
+		copy_to_unnamed(vim.v.event.regcontents)
+	end,
+})
